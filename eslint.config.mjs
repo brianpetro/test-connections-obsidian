05a189a2a0c5e06ae61b0b9d95dbcb10c18cfbeb
Brian Joseph Petro <brian@smartconnections.app>
2026-08-05 11:43:54 -0400

    up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,14 @@
 import shared_eslint_config from "../obsidian-smart-env/eslint.base.mjs";
 
-export default shared_eslint_config;
+export default [
+  ...shared_eslint_config,
+  {
+    files: ["src/**/*.js", "smart_env.config.js"],
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+    },
+  },
+];
+
